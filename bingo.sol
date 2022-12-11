8562c9aac80fa46bb7034eda6627f81788f3458a
KeystoneBF <keystonebarreto@gmail.com>
2022-12-10 23:06:07 -0300

    Adiciona função de reiniciar bingo e refatorando criação da cartela

--- bingo.sol
+++ bingo.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.5.0;
 contract Bingo {
     event GameStart(string message);
     event GameOver(string message);
-    event NewCard(uint cardId, uint[25] card);
+    event NewCard(uint cardId, uint[] card);
     event NewBallDrawn(uint number);
 
     address payable owner;
@@ -12,8 +12,7 @@ contract Bingo {
     bool started = false;
 
     uint[75] public drawnNumbers;
-    uint[25][] public cards;
-    uint[25] lastGeneratedCard;
+    uint[][] public cards;
 
     mapping (uint => address) public cardToOwner;
     mapping (address => uint) ownerCardCount;
@@ -75,6 +74,7 @@ contract Bingo {
     // função para criar uma nova cartela
     function _generateCard() private {
         uint counter = 0;
+        uint[] memory lastGeneratedCard = new uint[](25);
         for (uint column = 0; column < 5; column++) {
             for (uint line = 0; line < 5; line++) {
                 uint random = uint(keccak256(abi.encodePacked(block.timestamp, msg.sender))) % 15 + 15 * column;
@@ -114,20 +114,27 @@ contract Bingo {
     }
 
     // função para verificar se houve de fato um ganhador
-    function _isWinner(uint[25] memory _card) private view returns(bool) {
-        bool result = true;
+    function _isWinner(uint[] memory _card) private view returns(bool) {
         for(uint i = 0; i < _card.length; i++) {
             uint index = _card[i];
             if(drawnNumbers[index] == 0) {
-                result = false;
+                return false;
             }
         }
-        return result;
+        return true;
     }
 
     // função para o ganhador retirar seu prêmio
     /// @dev Por enquanto o prêmio está como 100% do valor arrecadado
     function withdrawPrize() public onlyWinner {
         winner.transfer(address(this).balance);
+    }
+
+    //função para reiniciar o bingo
+    function restartBingo() external onlyOwner {
+        started = false;
+        delete cards;
+        delete drawnNumbers;
+        winner = payable(address(0));
     }
 }
